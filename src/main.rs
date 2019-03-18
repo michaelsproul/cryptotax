18036f343c4d9da70006113f5c8b063782c4ca23
Michael Sproul <micsproul@gmail.com>
2019-03-18 22:38:42 +1100

    Refactor BTC markets code

--- src/main.rs
+++ src/main.rs
@@ -1,133 +1,17 @@
 #[macro_use]
 extern crate serde_derive;
 
-use csv::Trim;
-use itertools::Itertools;
+mod btc_markets;
+
 use std::io;
 use std::error::Error;
 
-#[derive(Debug, Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct Row {
-    creation_time: String,
-    record_type: String,
-    action: String,
-    currency: String,
-    // TODO: use rationals instead of floats?
-    amount: f64,
-    description: String,
-    reference_id: u32
-}
-
-fn get_rows() -> Result<Vec<Row>, Box<Error>> {
-    let mut rdr = csv::ReaderBuilder::new()
-        .trim(Trim::Headers)
-        .from_reader(io::stdin());
-
-    let mut rows = vec![];
-    for result in rdr.deserialize() {
-        rows.push(result?);
-    }
-
-    Ok(rows)
-}
-
-#[derive(Default, Debug, Serialize)]
-struct Trade {
-    creation_time: String,
-    reference_id: u32,
-    sold_currency: String,
-    sold_amount: f64,
-    bought_currency: String,
-    bought_amount: f64,
-    description: String,
-}
-
-impl Trade {
-    fn update_common_info(&mut self, row: &Row) {
-        if self.reference_id == 0 {
-            self.reference_id = row.reference_id;
-            self.description = row.description.clone();
-            self.creation_time = row.creation_time.clone();
-        }
-        assert_eq!(self.reference_id, row.reference_id);
-    }
-
-    fn add_buy_part(&mut self, currency: &str, amount: f64) {
-        assert!(amount >= 0.0);
-        if self.bought_currency == "" {
-            self.bought_currency = currency.into();
-        }
-        assert_eq!(self.bought_currency, currency);
-        self.bought_amount += amount;
-    }
-
-    fn add_sell_part(&mut self, currency: &str, amount: f64) {
-        assert!(amount >= 0.0);
-        if self.sold_currency == "" {
-            self.sold_currency = currency.into();
-        }
-        assert_eq!(self.sold_currency, currency);
-        self.sold_amount += amount;
-    }
-
-    fn add_txn_fee(&mut self, currency: &str, amount: f64) {
-        if currency == self.bought_currency {
-            self.bought_amount += -1.0 * amount;
-        } else if currency == self.sold_currency {
-            self.sold_amount += amount;
-        } else {
-            panic!("currency not one of the ones bought or sold in this txn: {} not in {}, {}",
-                currency, self.bought_currency, self.sold_currency
-            );
-        }
-    }
-
-    fn process_part(&mut self, part: &Row) {
-        self.update_common_info(part);
-
-        match part.record_type.as_str() {
-            "Trade" => self.process_trade(part),
-            "Fund Transfer" => self.process_transfer(part),
-            ty => panic!("Unknown record type: {}", ty),
-        }
-    }
-
-    fn process_trade(&mut self, part: &Row) {
-        if part.action == "Trading Fee" {
-            self.add_txn_fee(&part.currency, part.amount);
-        } else if part.amount >= 0.0 {
-            self.add_buy_part(&part.currency, part.amount);
-        } else {
-            self.add_sell_part(&part.currency, -1.0 * part.amount);
-        }
-    }
-
-    fn process_transfer(&mut self, _: &Row) {
-        // TODO: do something sensible here
-    }
-}
-
 fn run() -> Result<(), Box<Error>> {
-    let rows = get_rows()?;
-
-    let trades_grouped = rows
-        .into_iter()
-        .filter(|r| r.record_type == "Trade")
-        .group_by(|r| r.reference_id);
-
-    let mut trades = vec![];
-    for (_, txn_parts) in &trades_grouped {
-        let mut trade = Trade::default();
-        for part in txn_parts {
-            trade.process_part(&part);
-        }
-        trades.push(trade);
-    }
+    let btc_markets_trades = btc_markets::load_csv("data/btcmarkets.csv")?;
 
     // Write CSV to stdout
     let mut writer = csv::Writer::from_writer(io::stdout());
-    for trade in trades {
+    for trade in btc_markets_trades {
         writer.serialize(trade)?;
     }
 
